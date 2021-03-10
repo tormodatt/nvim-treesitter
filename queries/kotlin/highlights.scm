dda1e1b5d320e93ce9151d5fa8a1e88e8295416c
Tormod Alf Try Tufteland <tormodat.tufteland@eika.no>
2021-03-10 23:07:42 +0100

    fix highlights with tree-sitter 19

--- queries/kotlin/highlights.scm
+++ queries/kotlin/highlights.scm
@@ -69,10 +69,10 @@
 ;; Types
 (type_identifier) @type
 
-;; Annotations
-(annotation (single_annotation) @attribute)
-(single_annotation (user_type (type_identifier) @attribute))
-(single_annotation (constructor_invocation (user_type (type_identifier) @attribute)))
+;; TODO Annotations
+; (annotation (single_annotation) @attribute)
+; (single_annotation (user_type (type_identifier) @attribute))
+; (single_annotation (constructor_invocation (user_type (type_identifier) @attribute)))
 
 
 ;; it
